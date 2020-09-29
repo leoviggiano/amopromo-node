c45a241f7a4757f3968befc4fe2edf3071245a07
Léo Viggiano <leoviggiano1@outlook.com>
2020-09-29 03:50:11 -0300

    add: linting with eslint/prettier

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
 import express from 'express';
 import routes from './routes';
+//  require('dotenv/config');
 
 class App {
-    constructor() {
-        this.server = express();
-        this.middlewares();
-        this.routes();
-    }
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
 
-    middlewares() {
-        // Converter automaticamente toda requisição para formato JSON
-        this.server.use(express.json());
-    }
+  middlewares() {
+    // Converter automaticamente toda requisição para formato JSON
+    this.server.use(express.json());
+  }
 
-    routes() {
-        this.server.use(routes);
-    }
+  routes() {
+    // Utilização das rotas no arquivo routes
+    this.server.use(routes);
+  }
 }
 
 export default new App().server;
